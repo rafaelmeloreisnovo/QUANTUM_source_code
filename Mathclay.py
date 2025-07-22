49ff3e1974239438aac88079b394525b168638f4
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2025-07-22 20:40:52 -0300

    Update Mathclay.py

--- Mathclay.py
+++ Mathclay.py
@@ -7,3 +7,17 @@
 → gera 9 (var1, var2, var3)
 
 Clay maths = matemática viva, moldável, autorreferente
+olhar arquivos de ativos stocks e commodites
+
+data com hash dos xls cvs e o que deu no dia e hora
+
+98% previsão e accuracidade e confiança e 
+
+
+margens de erros 0.05%
+
+de 10 em 10 minutos com VALORES E tempo
+
+brasil e Japão e USD BTC eth
+
+AES rsa pacotes nao faco bruteforce é possível ler em pouco menos de 5seg
