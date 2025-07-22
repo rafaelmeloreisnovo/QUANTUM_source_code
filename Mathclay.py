4f444a1d5b0151efa7cb6104ae202cf0389a547c
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2025-07-22 20:36:46 -0300

    Create Mathclay.py
    Apenas para demonstrar os mais de trinta repositorios privados

---
+++ Mathclay.py
@@ -0,0 +1,9 @@
+1 dividido por 3 = 0.333... (não fecha em 1)
+→ erro quântico 0.000...1
+→ fractaliza, duplica, retroalimenta
+
+5 (símbolo do meio, , )
++ erro → expande
+→ gera 9 (var1, var2, var3)
+
+Clay maths = matemática viva, moldável, autorreferente
